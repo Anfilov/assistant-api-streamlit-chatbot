e54dd4088b7a467af54440d05505289dca8224af
Anfilov <67873602+Anfilov@users.noreply.github.com>
2024-01-23 23:02:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import openai
-from openai import OpenAIError  # Updated import for OpenAIError
+from openai import OpenAI
 import time
 import streamlit as st
 
@@ -12,11 +11,9 @@ def main():
 
     api_key = st.secrets["OPENAI_API_KEY"]
     assistant_id = st.secrets["ASSISTANT_ID"]
-    openai.api_key = api_key
 
     # Initiate st.session_state
-    if "client" not in st.session_state:
-        st.session_state.client = openai
+    st.session_state.client = OpenAI(api_key=api_key)
 
     if "messages" not in st.session_state:
         st.session_state.messages = []
@@ -25,7 +22,7 @@ def main():
         st.session_state.start_chat = False
 
     if st.session_state.client:
-        st.session_state.start_chat = True
+       st.session_state.start_chat = True
 
     if st.session_state.start_chat:
         # Display existing messages in the chat
@@ -33,8 +30,8 @@ def main():
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
 
-        # Accept user input
-        if prompt := st.chat_input(f"Zeptej se mě na cokoliv ohledně tvorby loga..."):
+                # Accept user input
+        if prompt := st.chat_input(f"Cokoliv o tvorbě loga..."):
             # Add user message to chat history
             st.session_state.messages.append({"role": "user", "content": prompt})
             # Display user message in chat message container
@@ -42,97 +39,86 @@ def main():
                 st.markdown(prompt)
 
             # Create a thread
-            st.session_state.thread = st.session_state.client.Thread.create()
+            st.session_state.thread = st.session_state.client.beta.threads.create()
 
             # Add a Message to the thread
-            st.session_state.client.Message.create(
+            st.session_state.client.beta.threads.messages.create(
                 thread_id=st.session_state.thread.id,
                 role="user",
                 content=prompt,
             )
 
-            try:
-                # Create a run and tell the assistant at which thread to look at
-                run = st.session_state.client.Run.create(
-                    thread_id=st.session_state.thread.id,
-                    assistant_id=assistant_id,
-                )
-            except OpenAIError as e:
-                st.error(f"An error occurred while creating a run: {e}")
-                return  # Stop execution if the run can't be created
+            # As of now, assistant and thread are not associated to eash other
+            # You need to create a run in order to tell the assistant at which thread to look at
+            run = st.session_state.client.beta.threads.runs.create(
+                thread_id=st.session_state.thread.id,
+                assistant_id=assistant_id,
+            )
+
+            # with while loop continuously check the status of a run until it neither 'queued' nor 'in progress'
+            def wait_for_complete(run, thread):
+                while run.status == "queued" or run.status == "in_progress":
+                    run = st.session_state.client.beta.threads.runs.retrieve(
+                        thread_id=thread.id,
+                        run_id=run.id,
+                    )
+                    time.sleep(0.5)
+                return run
 
             run = wait_for_complete(run, st.session_state.thread)
 
-            # Once the run has completed, list the messages in the thread
-            replies = st.session_state.client.Message.list(
+            # once the run has completed, list the messages in the thread -> they are ordered in reverse chronological order
+            replies = st.session_state.client.beta.threads.messages.list(
                 thread_id=st.session_state.thread.id
             )
 
-            # Process and display the response
+            # This function will parse citations and make them readable
+            def process_replies(replies):
+                citations = []
+
+                # Iterate over all replies
+                for r in replies:
+                    if r.role == "assistant":
+                        message_content = r.content[0].text
+                        annotations = message_content.annotations
+
+                        # Iterate over the annotations and add footnotes
+                        for index, annotation in enumerate(annotations):
+                            # Replace the text with a footnote
+                            message_content.value = message_content.value.replace(
+                                annotation.text, f" [{index}]"
+                            )
+
+                            # Gather citations based on annotation attributes
+                            if file_citation := getattr(
+                                annotation, "file_citation", None
+                            ):
+                                cited_file = st.session_state.client.files.retrieve(
+                                    file_citation.file_id
+                                )
+                                citations.append(
+                                    f"[{index}] {file_citation.quote} from {cited_file.filename}"
+                                )
+                            elif file_path := getattr(annotation, "file_path", None):
+                                cited_file = st.session_state.client.files.retrieve(
+                                    file_path.file_id
+                                )
+                                citations.append(
+                                    f"[{index}] Click <here> to download {cited_file.filename}"
+                                )
+
+                # Combine message content and citations
+                full_response = message_content.value + "\n" + "\n".join(citations)
+                return full_response
+
+            # Add the processed response to session state
             processed_response = process_replies(replies)
             st.session_state.messages.append(
                 {"role": "assistant", "content": processed_response}
             )
+            # Display assistant response in chat message container
             with st.chat_message("assistant"):
                 st.markdown(processed_response, unsafe_allow_html=True)
-
-
-def wait_for_complete(run, thread):
-    # Continuously check the status of a run until it neither 'queued' nor 'in progress'
-    while run.status == "queued" or run.status == "in_progress":
-        run = st.session_state.client.Run.retrieve(
-            thread_id=thread.id,
-            run_id=run.id,
-        )
-        time.sleep(0.5)
-    return run
-
-
-def process_replies(replies):
-    citations = []
-
-    # Iterate over all replies
-    for r in replies:
-        if r.role == "assistant":
-            message_content = r.content[0].text
-            annotations = message_content.annotations
-
-            # Iterate over the annotations and add footnotes
-            for index, annotation in enumerate(annotations):
-                # Replace the text with a footnote
-                message_content.value = message_content.value.replace(
-                    annotation.text, f" [{index}]"
-                )
-
-                # Handle file citations
-                if file_citation := getattr(annotation, "file_citation", None):
-                    if file_citation.file_id:
-                        cited_file = st.session_state.client.File.retrieve(
-                            file_citation.file_id
-                        )
-                        citations.append(
-                            f"[{index}] {file_citation.quote} from {cited_file.filename}"
-                        )
-                    else:
-                        # Log an error or display a warning message
-                        st.error(f"No file ID found for citation index {index}")
-
-                # Handle file paths
-                elif file_path := getattr(annotation, "file_path", None):
-                    if file_path.file_id:
-                        cited_file = st.session_state.client.File.retrieve(
-                            file_path.file_id
-                        )
-                        citations.append(
-                            f"[{index}] Click <here> to download {cited_file.filename}"
-                        )
-                    else:
-                        # Log an error or display a warning message
-                        st.error(f"No file ID found for file path index {index}")
-
-    # Combine message content and citations
-    full_response = message_content.value + "\n" + "\n".join(citations)
-    return full_response
 
 
 if __name__ == "__main__":
